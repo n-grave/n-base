71992f5d5f0122f149d99b001e48b330bdac6643
Matt Lockyer <mattdlockyer@gmail.com>
2025-05-07 10:10:52 -0700

    Remove spacing for split in compose yaml

--- contract/src/collateral.rs
+++ contract/src/collateral.rs
@@ -54,7 +54,7 @@ pub fn verify_codehash(raw_tcb_info: String, rtmr3: String) -> String {
     // event with compose hash matches report rtmr3
     require!(replayed_rtmr3 == rtmr3);
 
-    let (_, right) = app_compose.split_once("\\n        image:").unwrap();
+    let (_, right) = app_compose.split_once("image:").unwrap();
     let (left, _) = right.split_once("\\n").unwrap();
     let (_, codehash) = left.split_once("@sha256:").unwrap();
 
